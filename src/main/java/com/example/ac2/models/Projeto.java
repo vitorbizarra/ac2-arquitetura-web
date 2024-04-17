e275be3e26a986019843c9f9fba46dacc5513727
vitorbizarra <vitorbizarracontato@gmail.com>
2024-04-17 20:03:14 -0300

    :construction: wip: Model Projeto

---
+++ src/main/java/com/example/ac2/models/Projeto.java
@@ -0,0 +1,39 @@
+package com.example.ac2.models;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Projeto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 255, nullable = false)
+    @NonNull
+    private String descricao;
+
+    @Column(nullable = false)
+    @NonNull
+    private LocalDate dataInicio;
+
+    @Column(nullable = false)
+    @NonNull
+    private LocalDate dataFim;
+}
