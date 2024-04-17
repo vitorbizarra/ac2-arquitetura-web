06d03ad3f085f4c0fa2bf7e5dd9d025bef64e071
vitorbizarra <vitorbizarracontato@gmail.com>
2024-04-17 20:33:08 -0300

    :books: docs: Corrigindo comentários

--- src/main/java/com/example/ac2/repositories/FuncionarioRepository.java
+++ src/main/java/com/example/ac2/repositories/FuncionarioRepository.java
@@ -12,10 +12,6 @@ public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> 
      * No repositório ProjetoRepository crie um método que passamos o ID do
      * projeto e ele retornará os dados do projeto e a lista de funcionários
      * vinculados aquele projeto.
-     * 
-     * @Query("SELECT p from Projeto p LEFT JOIN FETCH p.funcionarios p WHERE p.id = :id"
-     * )
-     * Projeto findProjetoFetchFuncionarios(@Param("id") Long id);
      */
     @Query("SELECT f FROM Funcionario f LEFT JOIN FETCH f.projetos f WHERE f.id = :id")
     Funcionario findFuncionarioFetchProjetos(@Param("id") Long id);
